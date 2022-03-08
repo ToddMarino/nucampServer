c79787bbdd6ff789a04a8c03198200ff70da465b
ToddMarino <todd.j.marino@gmail.com>
2022-03-08 13:53:36 -0600

    Cookies and Sessions
    All sections are completed. Debug why logout doesn't destroy cookie.

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
+// const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const campsiteRouter = require("./routes/campsiteRouter");
+const promotionRouter = require("./routes/promotionRouter");
+const partnerRouter = require("./routes/partnerRouter");
+
 const mongoose = require("mongoose");
 
 const url = "mongodb://localhost:27017/nucampsite";
@@ -16,14 +25,8 @@ const connect = mongoose.connect(url, {
 connect.then(
   () => console.log("Connected correctly to server"),
   (err) => console.log(err)
-);
-
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const campsiteRouter = require("./routes/campsiteRouter");
-const promotionRouter = require("./routes/promotionRouter");
-const partnerRouter = require("./routes/partnerRouter");
-
+  );
+  
 const app = express();
 
 // view engine setup
@@ -33,28 +36,34 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitailized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.headers);
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-  }
+  console.log(req.session);
 
-  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  const user = auth[0];
-  const pass = auth[1];
-  if (user === 'admin' && pass === 'password') {
-      return next(); // authorized
+  if(!req.session.user) {    
+    const err = new Error('You are not authenticated!');
+    err.status = 401;
+    return next(err);
   } else {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');      
+    if (req.session.user === 'authenticated') {
+      return next();
+    } else {
+      const err = new Error('You are not authenticated');
       err.status = 401;
-      return next(err);
+      return next(err)
+    }
   }
 }
 
@@ -62,8 +71,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
